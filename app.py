b386a37406f5db3a9ab4f97764e6ede1ba08154e
YeasinFiyaz <yeasin.fiyaz@g.bracu.ac.bd>
2026-02-14 22:20:14 +0600

    Push latest updated project

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -16,7 +15,4 @@ def final():
     return render_template("final.html")
 
 if __name__ == "__main__":
-    # Get the port from Render environment variable, fallback to 5000 locally
-    port = int(os.environ.get("PORT", 5000))
-    # Bind to 0.0.0.0 so Render can access it
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
